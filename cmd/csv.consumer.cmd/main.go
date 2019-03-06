845000acc1335c5eac4d796ebd64b445944a3c74
Chris Reeves <hello@chris.reeves.io>
2019-03-06 10:27:24 +0000

    feat(csv): add signal listening

--- cmd/csv.consumer.cmd/main.go
+++ cmd/csv.consumer.cmd/main.go
@@ -8,9 +8,17 @@ import (
 	"io"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
-var csvpath = flag.String("csv", "", "CSV file to process") // nolint: gochecknoglobals
+var csvpath = flag.String("csv", "", "CSV file to process")
+
+var sigC = make(chan os.Signal, 1)
+
+type reader interface {
+	Read() ([]string, error)
+}
 
 // Checks errors, should us a proper logger
 func check(err error) {
@@ -26,10 +34,20 @@ func main() {
 	check(err)
 
 	r := csv.NewReader(bufio.NewReader(f))
+	go readCSV(r)
+
+	signal.Notify(sigC, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	sig := <-sigC
+	fmt.Println(sig)
+}
+
+func readCSV(r reader) {
 	for i := 0; ; i++ {
 		line, err := r.Read()
 		if err == io.EOF {
 			fmt.Println("Done")
+			break
 		}
 		check(err)
 		if i == 0 { // Skip the header
@@ -37,5 +55,4 @@ func main() {
 		}
 		fmt.Println(line)
 	}
-
 }
