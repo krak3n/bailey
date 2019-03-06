06fe466f66def8c40ae4888b7f7809fb6b79dd98
Chris Reeves <hello@chris.reeves.io>
2019-03-06 10:18:39 +0000

    feat(csv): basic csv line by line reader

--- cmd/csv.consumer.cmd/main.go
+++ cmd/csv.consumer.cmd/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+var csvpath = flag.String("csv", "", "CSV file to process") // nolint: gochecknoglobals
+
+// Checks errors, should us a proper logger
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
-	fmt.Println("csv.consumer.cmd")
+	flag.Parse()
+
+	f, err := os.Open(*csvpath)
+	check(err)
+
+	r := csv.NewReader(bufio.NewReader(f))
+	for i := 0; ; i++ {
+		line, err := r.Read()
+		if err == io.EOF {
+			fmt.Println("Done")
+		}
+		check(err)
+		if i == 0 { // Skip the header
+			continue
+		}
+		fmt.Println(line)
+	}
+
 }
